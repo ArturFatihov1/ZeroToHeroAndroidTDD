06f41d6da28a85566fc3b3d094a00782cc02fa07
Artur Fatihov <artur.fatihov23@mail.ru>
2024-10-04 22:47:06 +0500

    Task010 Test another solving

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -10,6 +10,7 @@ import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
     private var state: State = State.Initial
+
     private lateinit var textView: TextView
     private lateinit var linearLayout: LinearLayout
     private lateinit var button: Button
@@ -21,41 +22,43 @@ class MainActivity : AppCompatActivity() {
         linearLayout = findViewById(R.id.rootLayout)
         button.setOnClickListener {
             state = State.Removed
-            state.apply(linearLayout, textView)
-            button.isEnabled = false
+            state.apply(linearLayout, textView, button)
+
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putSerializable(KEY,state)
-        outState.putBoolean(BUT,button.isEnabled)
+        outState.putSerializable(KEY, state)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        state = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            savedInstanceState.getSerializable(KEY,State::class.java) as State
+        state = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            savedInstanceState.getSerializable(KEY, State::class.java) as State
         } else {
             savedInstanceState.getSerializable(KEY) as State
         }
-        state.apply(linearLayout,textView)
-        button.isEnabled = savedInstanceState.getBoolean(BUT)
+        state.apply(linearLayout, textView, button)
     }
+
     companion object {
         private const val KEY = "key"
-        private const val BUT = "state"
     }
 }
-interface State: Serializable {
-    fun apply(linearLayout: LinearLayout, textView: TextView)
-    object Initial: State {
-        override fun apply(linearLayout: LinearLayout, textView: TextView) = Unit
-    }
-    object Removed: State {
-        override fun apply(linearLayout: LinearLayout, textView: TextView) {
-            linearLayout.removeView(textView)
-        }
 
+interface State : Serializable {
+    fun apply(linearLayout: LinearLayout, textView: TextView,button: Button)
+
+    object Initial : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView, button: Button) = Unit
+    }
+
+    object Removed : State {
+
+        override fun apply(linearLayout: LinearLayout, textView: TextView, button: Button) {
+            linearLayout.removeView(textView)
+            button.isEnabled = false
+        }
     }
 }
